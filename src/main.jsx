0654588323ff12e8f2957959fa79160687afae34
Ajoy Paul <ajoypaulmindblue@gmail.com>
2025-04-25 15:17:56 +0530

    Update favicon to logo.png, add react-hook-form and react-toastify dependencies, and integrate ToastContainer in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Home from './components/Home.jsx'
+import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ToastContainer />
     <Home />
   </StrictMode>,
 )
